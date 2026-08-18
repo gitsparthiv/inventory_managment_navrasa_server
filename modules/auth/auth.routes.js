e86cb949fb47ce2fd27c64488c080abaf50eb07e
gitsparthiv <ghoshparthiv98@gmail.com>
2026-08-18 19:48:02 +0530

    created the middle ware and integrated in the routes now before any route comes next

--- modules/auth/auth.routes.js
+++ modules/auth/auth.routes.js
@@ -2,8 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const authController = require("./auth.controller");
+const authenticate = require("../../shared/middleware/auth.middleware");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
+
+router.get("/me", authenticate, (req, res) => {
+  res.status(200).json({
+    success: true,
+    user: req.user,
+  });
+});
 
 module.exports = router;
